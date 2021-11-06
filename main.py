4c91ad10e46421c5a32c63f8a0a00b489a87960b
Ttaha09 <75397556+Ttaha09@users.noreply.github.com>
2021-11-06 20:25:52 +0100

    new

---
+++ main.py
@@ -0,0 +1,81 @@
+import torch.nn as nn
+import torch
+from torchvision import models,transforms
+import streamlit as st
+from PIL import Image
+from matplotlib import cm as c
+import numpy as np
+import matplotlib.pyplot as plt
+
+######## Model ##########
+
+def make_layers(cfg, in_channels = 3,batch_norm=False,dilation = False):
+    if dilation:
+        d_rate = 2
+    else:
+        d_rate = 1
+    layers = []
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=d_rate, dilation=d_rate)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+class CSRNet(nn.Module):
+    def __init__(self, load_weights=False):
+        super(CSRNet, self).__init__()
+        self.seen = 0
+        self.frontend_feat = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512]
+        self.backend_feat = [512, 512, 512, 256, 128, 64]
+        self.frontend = make_layers(self.frontend_feat)
+        self.backend = make_layers(self.backend_feat,in_channels = 512,dilation = True)
+        self.output_layer = nn.Conv2d(64, 1, kernel_size=1)
+        if not load_weights:
+            mod = models.vgg16(pretrained = True)
+            self._initialize_weights()
+            items = list(self.frontend.state_dict().items())
+            _items = list(mod.state_dict().items())
+            for i in range(len(self.frontend.state_dict().items())):
+                items[i][1].data[:] = _items[i][1].data[:]
+    def forward(self,x):
+        x = self.frontend(x)
+        x = self.backend(x)
+        x = self.output_layer(x)
+        return x
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, std=0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+m = nn.MaxPool2d(2, stride=2)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = CSRNet().to(device)
+PATH = "./Model/crowd-counting.pth"
+model.load_state_dict(torch.load(PATH))
+
+transform=transforms.Compose([
+                      transforms.ToTensor(),transforms.Normalize(
+                          mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225]),
+                  ])
+
+uploaded_file = st.file_uploader('Select an Image', ['png', 'jpg'])
+
+if uploaded_file is not None:
+    img = transform(Image.open(uploaded_file).convert('RGB')).to(device)
+    output = model(img[None])
+    st.write("Predicted Count : "+str(int(m(output).detach().cpu().sum().numpy())))
+    temp = np.asarray(output.detach().cpu().reshape(output.detach().cpu().shape[2],output.detach().cpu().shape[3]))
+    plt.imshow(temp,cmap = c.jet)
+    plt.show()
